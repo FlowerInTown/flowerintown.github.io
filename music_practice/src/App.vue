a6d0961fc6d45719df12bc081af55cda7b13bbc1
song.tang <song.tang@happyelements.com>
2025-12-25 14:10:56 +0800

    init

---
+++ music_practice/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    <h1>音乐练习工具</h1>
+    <p>Vue + GitHub Pages</p>
+  </div>
+</template>
+
+<style>
+.app {
+  padding: 40px;
+  font-family: Arial, sans-serif;
+}
+</style>
